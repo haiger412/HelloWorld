849fd9c6a0a4cc86cbf61a7b2950ea837aa3a944
zhangsan <zhangsan@gitblit.com>
2019-05-03 14:46:37 +0800

    项目收工

--- StartUp.java
+++ StartUp.java
@@ -1,6 +1,7 @@
 public class StartUp{
 	public static void main(String ...agrs){
-		Hello.sayHello("好嗨哦，感觉声明已经到达了巅峰");
+		Hello.sayHello("好嗨哦，感觉生命已经到达了巅峰");
+		Hello.sayHello("好嗨哦，感觉人生已经到达了高潮");
 	}
 	
 }
